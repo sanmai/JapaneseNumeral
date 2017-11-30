38d3a6a962250f2782c76abcc7237d79289eae59
Alexey Kopytko <alexey@kopytko.com>
2017-11-30 11:20:33 +0900

    Namespaces and tests

---
+++ tests/JapaneseNumerals/FromArabicToJapaneseTest.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * Copyright 2014 OFTN Inc.
+ * Copyright 2017 Alexey Kopytko <alexey@kopytko.com>
+ *
+ * Licensed under the MIT License; see LICENSE for details.
+ */
+
+namespace JapaneseNumerals;
+
+class FromArabicToJapaneseTest extends TestCase
+{
+    /**
+     * @dataProvider dataProvider
+     *
+     * @param mixed $japanese
+     * @param mixed $arabic
+     */
+    public function testConvert($japanese, $arabic)
+    {
+        $this->assertEquals($japanese, JapaneseNumerals::fromArabicToJapanese($arabic));
+    }
+}
---
+++ tests/JapaneseNumerals/FromJapaneseToArabicTest.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * Copyright 2014 OFTN Inc.
+ * Copyright 2017 Alexey Kopytko <alexey@kopytko.com>
+ *
+ * Licensed under the MIT License; see LICENSE for details.
+ */
+
+namespace JapaneseNumerals;
+
+class FromJapaneseToArabicTest extends TestCase
+{
+    /**
+     * @dataProvider dataProvider
+     *
+     * @param mixed $japanese
+     * @param mixed $arabic
+     */
+    public function testConvert($japanese, $arabic)
+    {
+        $this->assertEquals($arabic, JapaneseNumerals::fromJapaneseToArabic($japanese));
+    }
+}
